d6688e035840d097a7d35948e76fb7044a9c9325
Padma2Code <padmavalli.aluri@gmail.com>
2021-01-27 23:56:58 +0000

    fetching tweets with Ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,3 +1,4 @@
+
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
@@ -5,33 +6,7 @@
  */
 
 
- // Test / driver code (temporary). Eventually will get this from the server.
- const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-  
-  const renderTweets = function(tweets) {
+const renderTweets = function(tweets) {
     return tweets.forEach(tweet => {
       $('#tweets-container').append(createTweetElement(tweet));
     });
@@ -65,8 +40,24 @@
     return element;
   }
   
+  const loadTweets = (url, method, cb) => {
+    $.ajax({
+      url,
+      method,
+    })
+      .done(data => {
+        cb(data);
+      })
+      .fail(err => {
+        console.log('Error:', err)
+      })
+      .always( () => {
+        console.log("Tweets loaded!");
+      });
+  };
+  
   $(document).ready(function() {
-    renderTweets(data);
+    loadTweets("/tweets", "GET", renderTweets);
   
     $("form").on("submit", function() {
       event.preventDefault();
@@ -89,4 +80,3 @@
         });
     });
   });
-  
