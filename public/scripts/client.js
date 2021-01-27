03ff7f0c1af58a628adf336f0d9b3c7d30d34a8c
Padma2Code <padmavalli.aluri@gmail.com>
2021-01-27 18:14:19 +0000

    implement the renderTweets function and update the style of tweets that are returned

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,3 +3,67 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+//from initial-tweets.json file
+const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+  
+  const renderTweets = function(tweets) {
+    return tweets.forEach(tweet => {
+      $('#tweets-container').append(createTweetElement(tweet));
+    });
+  };
+  
+  const createTweetElement = function(tweetObj) {
+    const element = `
+      <article class="tweet">
+      <header>
+        <div class="wrapper">
+          <img src=${tweetObj.user.avatars} />
+          <span class="name">${tweetObj.user.name}</span>
+        </div>
+        <span class="handle">${tweetObj.user.handle}</span>
+      </header>
+      <div class="content">
+          ${tweetObj.content.text}
+      </div>
+      <footer>
+        <span class="date">
+        ${tweetObj.created_at}
+        </span>
+        <div class="actions">
+          <img src="/images/flag.png">
+          <img src="/images/retweet-symbol.png">
+          <img src="/images/like-symbol.png">
+        </div>
+      </footer>
+      </article>
+    `;
+    return element;
+  }
+  
+  $(document).ready(function() {
+    renderTweets(data);
+  });
+  
