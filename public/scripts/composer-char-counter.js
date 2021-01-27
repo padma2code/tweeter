2b37e3b2c20d6c6841cb28d546281d6cae4b29b3
Padma2Code <padmavalli.aluri@gmail.com>
2021-01-27 05:18:50 +0000

    pushing all work for tuesday

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+
+  $(document).ready(function() {
+  
+    $('textarea').on('input', function() {
+      const maxCount = 140;
+      const inputLength = $(this).val().length;
+      $(this).nextAll(".counter").text(maxCount - inputLength);
+      if (maxCount - inputLength < 0) {
+        $(this).nextAll(".counter").css("color", "red");
+      } else {
+        $(this).nextAll(".counter").css("color", "#545149");
+      }
+    });
+    
+  });
+  
